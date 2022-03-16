6c3b1f63daaf2ee35ed3eee15346ed7cf81336e9
Henry Olders <henry@olders.ca>
2022-03-16 09:32:28 -0400

    added ci.yml workflow; added arrhythmia.arff dataset

--- temp_test.v
+++ temp_test.v
@@ -6,7 +6,7 @@ import os
 // import tools
 
 fn testsuite_begin() ? {
-	println(os.execute_or_panic('v up\nv update holder66.hamnn'))
+	// println(os.execute_or_panic('v up\nv update holder66.hamnn'))
 	if os.is_file('vhamnn') {
 		os.rm('vhamnn') ?
 	}
@@ -26,11 +26,11 @@ fn testsuite_end() ? {
 
 // test_verify
 fn test_verify() {
-	println(os.execute_or_panic('./vhamnn verify -h'))
-	println(os.execute_or_panic('./vhamnn verify -c -t datasets/bcw174test datasets/bcw350train'))
-	// save a classifier to a file
-	println(os.execute_or_panic('./vhamnn make -a 33 -b 2,16 -w -o tempfolder/soybean.cl datasets/soybean-large-train.tab'))
-	println(os.execute_or_panic('./vhamnn verify -c -w -s -k tempfolder/soybean.cl -t datasets/soybean-large-test.tab'))
+	// println(os.execute_or_panic('./vhamnn verify -h'))
+	// println(os.execute_or_panic('./vhamnn verify -c -t datasets/bcw174test datasets/bcw350train'))
+	// // save a classifier to a file
+	// println(os.execute_or_panic('./vhamnn make -a 33 -b 2,16 -w -o tempfolder/soybean.cl datasets/soybean-large-train.tab'))
+	// println(os.execute_or_panic('./vhamnn verify -c -w -s -k tempfolder/soybean.cl -t datasets/soybean-large-test.tab'))
 	// println(os.execute_or_panic('./vhamnn explore -g -f 0 -c -a 3,4 -b 6,8  datasets/prostata.tab'))
 	// println(os.execute_or_panic('./vhamnn explore -g -e -c -o tempfolder/breast.exr datasets/breast-cancer-wisconsin-disc.tab'))
 }
